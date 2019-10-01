08cf41f36648d828f5749193046a603de48c758d
ilyakrasnou <ilyakr1999@yandex.by>
2019-10-01 06:34:14 +0300

    Add some classes of model

---
+++ FreeRockSol/FreeRock/Models/Album.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace FreeRock.Models
+{
+    public class Album
+    {
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public List<string> Genre;
+        public string Description { get; set; }
+        public int ArtistId { get; set; }
+
+    }
+
+    public class AlbumContext : DbContext
+    {
+        public AlbumContext(DbContextOptions<AlbumContext> options):base(options)
+        {
+
+        }
+        public DbSet<Album> Albums { get; set; }
+    }
+}
